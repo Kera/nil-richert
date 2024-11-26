50628c8a82634ba74e749278c9dfe2e09e5bd459
Nil <contact@nil-richert.fr>
2024-11-26 16:17:30 +0100

    update scss rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/scss/_main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern',
+        },
+      },
+    },
+  },
   modules: ['@nuxt/image']
 })
