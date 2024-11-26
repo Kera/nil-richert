171a5b248a78673eafeb55d6d0922c22b853c5ff
Nil <nil.richert@razorfish.fr>
2024-11-26 15:30:44 +0100

    Waiting website

---
+++ pages/index/index.vue
@@ -0,0 +1,16 @@
+<script>
+  import HomeLogoVue from "~/components/organims/HomeLogo/HomeLogo.vue";
+
+  export default {
+    name: 'Home',
+    components: {
+      HomeLogoVue,
+    }
+  }
+</script>
+
+<template>
+  <section>
+    <HomeLogoVue />
+  </section>
+</template>
